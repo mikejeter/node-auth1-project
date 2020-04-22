4e61a690daf8f9e678e633b43d82034af4bc1826
mikejeter <mjeter45@yahoo.com>
2020-04-22 19:10:00 -0400

    Created users router, auth router added data to database

--- api/server.js
+++ api/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-// const usersRouter = require("../users/users-router.js");
+const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/auth-router.js");
 
 const server = express();
 
@@ -10,7 +11,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use("/api/users", usersRouter);
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
---
+++ auth/auth-router.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 16);
+
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json({ saved });
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'problem with the database', error: err});
+        });
+});
+
+router.post("/login", (req, res) => {
+    const {username, password} =req.body;
+
+    Users.findBy({username})
+    .then(([user]) => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            res.status(200).json({ message: "welcome!"}); 
+        } else {
+            res.status(401).json({ message: 'invalid creds'});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'problem with the database', error: err});
+    });
+});
+
+module.exports = router;
